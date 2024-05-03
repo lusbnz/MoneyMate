d872bde4eef392a5531b462a5500fa621a069fd7
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-05-03 22:24:04 +0700

    refix

--- app/api/user-settings/route.ts
+++ app/api/user-settings/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
